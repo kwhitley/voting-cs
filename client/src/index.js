a6f2a4aca7a1bb541a5219eb58e15690f542ed1d
Kevin Whitley <kevin3503@gmail.com>
2016-08-09 14:51:26 -0500

    ready to implement own ReduxFactory

--- client/src/index.js
+++ client/src/index.js
@@ -2,9 +2,13 @@ require('./styles/app.scss');
 
 import React from 'react';
 import ReactDOM from 'react-dom';
-import {createStore, applyMiddleware} from 'redux';
-import {Provider} from 'react-redux';
+// import {createStore, applyMiddleware} from 'redux';
+// import {Provider} from 'react-redux';
 import TodoList from './components/TodoList';
+
+import * as actionCreators from './todolist.redux.js';
+console.log(actionCreators);
+// window.Cars = Cars;
 
 // render router
 ReactDOM.render(
